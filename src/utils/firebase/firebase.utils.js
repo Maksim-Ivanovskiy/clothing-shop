d72afe638e29c46393e2f04cd298ef5730d5eeb4
Maksim Ivanovskiy <ivanovskiy.maxim@mail.ru>
2022-08-29 18:44:37 -0400

    added functionality to sign up components

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,7 +3,8 @@ import {
     getAuth, 
     signInWithRedirect, 
     signInWithPopup, 
-    GoogleAuthProvider 
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword
 } from 'firebase/auth';
 import {
     getFirestore,
@@ -35,7 +36,8 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googlePro
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+    if(!userAuth) return;
     const userDocRef = doc(db, 'users', userAuth.uid);
 
     const userSnapshot = await getDoc(userDocRef);
@@ -48,7 +50,8 @@ export const createUserDocumentFromAuth = async (userAuth) => {
             await setDoc(userDocRef, {
                 displayName, 
                 email, 
-                createdAt
+                createdAt,
+                ...additionalInformation
             });  
         } catch (error) {
             console.log('error creating the user', error.message);
@@ -57,3 +60,9 @@ export const createUserDocumentFromAuth = async (userAuth) => {
 
     return userDocRef;
 };
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return; 
+
+    return await createUserWithEmailAndPassword(auth, email, password)
+}
