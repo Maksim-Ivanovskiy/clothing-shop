c3d3c35c2987dad3bc088914271ecddbebbf331a
Maksim Ivanovskiy <ivanovskiy.maxim@mail.ru>
2022-09-02 20:06:09 -0400

    working on category map

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -15,7 +15,9 @@ import {
     getDoc,
     setDoc,
     collection,
-    writeBatch
+    writeBatch,
+    query,
+    getDocs
 } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -50,8 +52,22 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
         batch.set(docRef, object);
     });
 
-    await batch.commit();
+    await batch.commit();   
 };
+
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+        const { title, items } = docSnapshot.data();
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {});
+
+    return categoryMap;
+}
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
     if(!userAuth) return;
