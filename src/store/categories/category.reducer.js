b81e255020de9d373fc946ddd02996fd520237a7
Maksim Ivanovskiy <ivanovskiy.maxim@mail.ru>
2022-09-12 19:05:02 -0400

    created cateories folder and deleted user context

---
+++ src/store/categories/category.reducer.js
@@ -0,0 +1,18 @@
+/* import { act } from "react-dom/test-utils" */
+import { CATEGORIES_ACTION_TYPES } from "./category.types"; 
+
+
+export const CATEGORIES_INITIAL_STATE = {
+    categoriesMap: {}
+}
+
+export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action = {}) => {
+    const { type, payload } = action;
+
+    switch(type) {
+        case CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP:
+            return {...state, categoriesMap: payload};
+        default: 
+            return state;
+    }
+};
